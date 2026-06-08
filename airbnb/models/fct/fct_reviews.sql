0e2f995a11df39489065fb4e8781e5026efa4dc1
Patryk Matychniak <patrykmatychniak@gmail.com>
2026-06-08 20:21:55 +0200

    variables

--- airbnb/models/fct/fct_reviews.sql
+++ airbnb/models/fct/fct_reviews.sql
@@ -15,5 +15,12 @@ SELECT
 FROM src_reviews
 WHERE review_text IS NOT NULL
 {% if is_incremental() %}
+    {% if var("start_date", False) and var("end_date", False) %}
+    {{ log('Loading ' ~ this ~ ' incrementally (start_date: ' ~ var("start_date") ~ ', end_date: ' ~ var("end_date") ~ ')', info=True ) }}
+    AND review_date >= '{{ var("start_date") }}'
+    AND review_date < '{{ var("end_date") }}'
+    {% else %}
     AND review_date > (SELECT MAX(review_date) FROM {{ this }})
+    {{ log('Loading ' ~ this ~ ' incrementally (all missing dates)', info=True) }}
+    {% endif %}
 {% endif %}
